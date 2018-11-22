8b20867bdf1283e9eff8446ed8f95d759a124d4d
sofieriisberg <sofieriisberg@hotmail.com>
2018-11-22 15:32:29 +0100

    Extra parameters in User model class

--- app/src/main/java/com/example/wewish/User.java
+++ app/src/main/java/com/example/wewish/User.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public class User {
     private String userName;
+    private String birthDate;
+    private String email;
     private List<Wish> wishList;
+    private List<String> subscriberList;
 
     public User(String userName, List<Wish> wishList) {
         this.userName = userName;
@@ -28,5 +31,29 @@ public class User {
 
     public void setWishList(List<Wish> wishList) {
         this.wishList = wishList;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getSubscriberList() {
+        return subscriberList;
+    }
+
+    public void setSubscriberList(List<String> subscriberList) {
+        this.subscriberList = subscriberList;
     }
 }
